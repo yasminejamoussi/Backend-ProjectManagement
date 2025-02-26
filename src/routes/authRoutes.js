bede019df714abba25fde3c959af78203371f5ba
ranime-sb <ranime.sboui@esprit.tn>
2025-02-26 18:20:22 +0100

    2FA+RBAC

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
-const { googleAuth,sendResetCode,verifyResetCode,resetPassword ,loginWithFace } = require('../controllers/authController');
+const { googleAuth,sendResetCode,verifyResetCode,resetPassword ,loginWithFace ,generate2FA,} = require('../controllers/authController');
 
 // Auth Routes
 router.post("/register", userController.register);
@@ -11,6 +11,7 @@ router.get("/profile", authMiddleware, userController.getProfile);
 router.get("/google", googleAuth);
 router.post('/login-with-face',loginWithFace );
 router.post("/register-face-label", userController.registerFaceLabel);
+
 // Password Reset Routes
 router.post("/forgot-password", sendResetCode);
 router.post("/verify-code", verifyResetCode);
@@ -20,5 +21,11 @@ router.get("/users", userController.getUsers);
 router.get("/users/:id", userController.getUserById); 
 router.put("/users/:id", userController.updateUser); 
 router.delete("/users/:id",userController.deleteUser);
+
+//2FA
+router.post("/generate-2fa", generate2FA);  
+router.post("/enable-2fa",userController.enable2FA);  
+router.post("/verify-2fa", userController.verify2FA);  
+
 
 module.exports = router;
