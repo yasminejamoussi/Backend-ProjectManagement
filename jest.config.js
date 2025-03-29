d95024186ffbf956e9ad98bd3199ea51758a9b8a
ranime-sb <ranime.sboui@esprit.tn>
2025-03-29 19:25:06 +0100

    picicd

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   coverageReporters: ["lcov", "text", "html"],
   reporters: [
       "default",
-      ["jest-junit", { outputDirectory: "test-results", outputName: "results.xml" }]
+      ["jest-junit", { outputDirectory: "test-results", outputName: "jest-junit.xml",includeConsoleOutput: true
+      }]
   ],
   testMatch: ["**/test/**/*.unit.test.js"],
   setupFilesAfterEnv: ["<rootDir>/test/setup.js"]
--- test/setup.js
+++ test/setup.js
@@ -1,32 +1,33 @@
-const mongoose = require("mongoose");
-
-// Mock complet de Mongoose avec Schema.Types
 jest.mock('mongoose', () => {
-  const actualMongoose = jest.requireActual('mongoose');
-  return {
-    ...actualMongoose,
-    connect: jest.fn().mockResolvedValue({}),
-    Schema: {
-      Types: {
-        ObjectId: actualMongoose.Schema.Types.ObjectId // <-- Correction clé
-      }
-    },
-    connection: {
-      readyState: 1,
-      dropDatabase: jest.fn().mockResolvedValue(true),
-      close: jest.fn().mockResolvedValue(true),
-      collections: {
-        users: { deleteMany: jest.fn().mockResolvedValue({}) }
-      }
-    },
-    model: jest.fn().mockImplementation(() => ({
-      find: jest.fn().mockReturnThis(),
-      findOne: jest.fn().mockReturnThis(),
-      save: jest.fn().mockResolvedValue({}),
-      exec: jest.fn()
-    }))
-  };
+    const actualMongoose = jest.requireActual('mongoose');
+    const Schema = actualMongoose.Schema;
+  
+    return {
+      ...actualMongoose,
+      connect: jest.fn().mockResolvedValue({}),
+      Schema: Object.assign(function (definition) {
+        return new Schema(definition);
+      }, {
+        ...Schema,
+        Types: {
+          ObjectId: actualMongoose.Schema.Types.ObjectId
+        }
+      }),
+      connection: {
+        readyState: 1,
+        dropDatabase: jest.fn().mockResolvedValue(true),
+        close: jest.fn().mockResolvedValue(true),
+        collections: {
+          users: { deleteMany: jest.fn().mockResolvedValue({}) }
+        }
+      },
+      model: jest.fn().mockImplementation(() => ({
+        find: jest.fn().mockReturnThis(),
+        findOne: jest.fn().mockReturnThis(),
+        save: jest.fn().mockResolvedValue({}),
+        exec: jest.fn()
+      }))
+    };
 });
-
 beforeEach(() => jest.clearAllMocks());
 afterAll(() => jest.restoreAllMocks());
