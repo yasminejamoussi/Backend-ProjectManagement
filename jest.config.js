ca71a9fd2fa151cc53b8df62961ee3c05147ae25
ranime-sb <ranime.sboui@esprit.tn>
2025-05-03 22:52:34 +0200

    docker

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     testEnvironment: "node",
     collectCoverage: true,
     coverageDirectory: "coverage",
+    coverageReporters: ["lcov", "text"], 
     reporters: [
       "default",
       ["jest-junit", { outputDirectory: "test-results", outputName: "results.xml" }]
