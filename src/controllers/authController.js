60da88935fe966a2a37102aa261d27a2f35347e0
ranime-sb <ranime.sboui@esprit.tn>
2025-05-10 14:39:14 +0200

    cicd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -561,7 +561,7 @@ const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+        pass: process.env.EMAIL_PASS
     }
 });
 
